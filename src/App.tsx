4849876bb802bd1a526838d69a8d97abdc1bebba
Tokan Vlad <vlad.tokan96@mail.ru>
2023-09-19 14:14:01 +0300

    штош

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import AppRouter from "./AppRouter";
+import Header from "./components/Header/Header";
+
+// @ts-ignore
+import {store} from 'src/store';
+
 import './App.css';
+import {PayWall} from "./utils/constants";
 
 function App() {
+  const [loc, setLoc] = useState(false);
+
+  useEffect(() => {
+    setLoc(window.location.href.includes(PayWall));
+  }, [window.location.href]);
   return (
-    <div/>
+    <Provider store={store}>
+      <BrowserRouter>
+        {
+          !loc
+            ? <Header />
+            : null
+        }
+        <AppRouter />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
